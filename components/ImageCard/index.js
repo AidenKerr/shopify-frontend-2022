5d20134f0af585eebb6fe813d266da4ecc79eda3
Aiden Kerr <aiden.k2000@hotmail.com>
2022-01-11 21:37:48 -0800

    add styling, mobile support, and formatting date

--- components/ImageCard/index.js
+++ components/ImageCard/index.js
@@ -1,19 +1,28 @@
 import Image from "next/image";
 import styles from "./ImageCard.module.css";
 
+const getDate = (dateStr) => {
+  const date = new Date(dateStr);
+  const options = {year: 'numeric', month: 'long', day: 'numeric'}
+  return date.toLocaleDateString('en-US', options);
+}
+
 export function ImageCard({ photo }) {
   if (!photo) return null; // todo change to loading cards
+
   console.log(photo);
-  const { img_src, rover, earth_date } = photo || {};
+  const { img_src, rover, earth_date, camera } = photo || {};
   const { name } = rover || {};
+  const { full_name } = camera || {};
 
   return (
     <div className={styles.card}>
       <div className={styles.image}>
         <Image width={300} height={300} src={img_src} />
       </div>
-      <p>By {name} </p>
-      <time dateTime={earth_date}>{earth_date}</time>
+      <h4>{full_name}</h4>
+      <h5>By {name}</h5>
+      <p><time dateTime={earth_date}>{getDate(earth_date)}</time></p>
     </div>
   );
 }
