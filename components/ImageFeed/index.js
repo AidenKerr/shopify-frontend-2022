8aa4503d82c302cc269992f867278f74e91ddd80
Aiden Kerr <aiden.k2000@hotmail.com>
2022-01-11 21:53:20 -0800

    add key to image card list

--- components/ImageFeed/index.js
+++ components/ImageFeed/index.js
@@ -7,7 +7,9 @@ export function ImageFeed() {
   // todo move to env variables
   const key = "n8Tn91dkpKCXx1cLHb6BFmBTqjcWdaWJHoVOwADC";
   useEffect(() => {
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${key}`)
+    fetch(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${key}`
+    )
       .then((res) => {
         if (res.ok) {
           return res.json();
@@ -22,5 +24,11 @@ export function ImageFeed() {
       });
   }, []);
 
-  return [...Array(10).keys()].map(n => <ImageCard photo={photos[n]}/>);
+  return (
+    <div>
+      {[...Array(10).keys()].map((n) => (
+        <ImageCard photo={photos[n]} key={n} />
+      ))}
+    </div>
+  );
 }
